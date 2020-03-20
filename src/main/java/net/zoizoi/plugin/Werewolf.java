a01327fea21f577b1dd8fdf7017508d4f969958e
ZOI <zoizoichannel@gmail.com>
2020-03-20 20:32:37 +0900

    初期設定

--- src/main/java/net/zoizoi/plugin/Werewolf.java
+++ src/main/java/net/zoizoi/plugin/Werewolf.java
@@ -7,11 +7,13 @@ public final class Werewolf extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Werewolf-Plugin が有効になりました。");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Werewolf-Plugin が無効になりました。");
     }
 }
