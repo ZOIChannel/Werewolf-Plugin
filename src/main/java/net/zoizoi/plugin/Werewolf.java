84919a7a3899c9daadbaf68e8411467cd5461db7
ZOI <zoizoichannel@gmail.com>
2020-03-20 20:16:55 +0900

    setup

---
+++ src/main/java/net/zoizoi/plugin/Werewolf.java
@@ -0,0 +1,17 @@
+package net.zoizoi.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Werewolf extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
