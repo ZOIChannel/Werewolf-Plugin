0bf2d46b2c0617d0e189b275b9d6b6432f9034d2
ZOI <zoizoichannel@gmail.com>
2020-03-21 18:56:53 +0900

    ちょっとした

--- src/main/java/net/zoizoi/plugin/Werewolf.java
+++ src/main/java/net/zoizoi/plugin/Werewolf.java
@@ -12,8 +12,22 @@ public final class Werewolf extends JavaPlugin {
     }
 
     @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        // プレイヤーが「/basic」コマンドを投入した際の処理...
+        if(cmd.getName().equalsIgnoreCase("wolf-start")){
+            // 何かの処理
+            plg.getLogger().info("Game Start!!!");
+            return true;
+            // コマンドが実行された場合は、trueを返して当メソッドを抜ける。
+        }
+        return false;
+        // コマンドが実行されなかった場合は、falseを返して当メソッドを抜ける。
+    }
+    
+    @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Werewolf-Plugin が無効になりました。");
     }
+
 }
